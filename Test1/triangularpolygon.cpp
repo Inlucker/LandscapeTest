35006cc418a2d6c03e0604e9b3b91f954f82d7a0
Inlucker <Arseny707@mail.ru.com>
2021-08-08 02:16:52 +0300

    color variant2

--- Test1/triangularpolygon.cpp
+++ Test1/triangularpolygon.cpp
@@ -256,16 +256,17 @@ void TriangularPolygon::calcColor()
     double cosinus = fabs(B/sqrt(A*A+B*B+C*C));
 
     color = Qt::darkGreen;//QColor(200, 200, 200);
-    float h, s, l;
+    /*float h, s, l;
     color.getHslF(&h, &s, &l);
     //cout << h << "; " << s << "; " << l << endl;
-    /*if (cosinus > 1)
+    if (cosinus > 1)
     {
         cout << normal << endl;
         cout << beam << endl;
         cout << cosinus << endl;
     }*/
-    color.setHslF(color.hslHueF(), color.hslSaturationF(), 0.25+float(cosinus/4)); //1? = lightness, 0? = drakness
+    //color.setHslF(color.hslHueF(), color.hslSaturationF(), 0.25+float(cosinus/4)); //1? = lightness, 0? = drakness
+    color.setHsvF(color.hsvHueF(), color.hsvSaturationF(), 0.25+float(cosinus/4)); //1? = lightness, 0? = drakness
 }
 
 ostream& operator <<(ostream& os, const TriangularPolygon& pol)
